4bbd4da782f5601119a3962e195b96908b7e00fb
Jassim ALHUMIAD <s202257400@kfupm.edu.sa>
2026-03-30 10:07:25 +0300

    completed TODO4

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,26 +1,21 @@
 // TODO 1: Import Express
-import express from 'express';
-
+import express from "express";
 
 // TODO 2: Create the Express app and store it in a variable named app
 const app = express();
 
-
 // TODO 3: Allow React to access the server
-import cors from 'cors';
+import cors from "cors";
 app.use(cors());
-
 
 // TODO 5: Create the home route "/"
 
-
-
 // TODO 6: Create the "/about" route
-
-
 
 // TODO 7: Create the "/student" route
 
-
-
 // TODO 4: Start the server on port 3000
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+  console.log("http://localhost:3000");
+});
